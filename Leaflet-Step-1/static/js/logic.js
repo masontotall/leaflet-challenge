5164e1d7dfe2f60e12c9db5b8ef6db88ca5e7376
masontotall <68935404+masontotall@users.noreply.github.com>
2021-06-13 15:56:38 -0500

    Think I have most of the js logic complete still need to write a function for marker size and color and get it to show up on my page

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,2 +1,81 @@
 // Store API link
 var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
+
+
+//Function to create marker size and color based on magnitude of earthquake
+
+// Perform a GET request to the query URL
+d3.json(link, function(data) {
+    
+    createFeatures(data.features);
+  });
+  
+  function createFeatures(earthquakeData) {
+  
+    var earthquakes = L.geoJSON(earthquakeData, {
+    // Define a function for each feature in feature array
+
+    // Give each feature the popup info on the earthquake
+   onEachFeature : function (feature, layer) {
+  
+      layer.bindPopup("<h3>" + feature.properties.place +
+        "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" + "<p> Magnitude: " +  feature.properties.mag + "</p>")
+      },     pointToLayer: function (feature, latlng) {
+
+        return new L.circle(latlng,
+          {radius: markerSize(feature.properties.mag),
+          fillColor: markerColor(feature.properties.mag),
+          fillOpacity: 1,
+          stroke: false,
+      })
+    }
+    });
+// Send earthquakes to createMap
+
+createMap(earthquakes);
+}
+
+function createMap(earthquakes) {
+
+    // Satelite and Darkmap layers
+
+    var satmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 20,
+    id: "mapbox.satellite",
+    accessToken: API_KEY
+  });
+
+    var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 20,
+    id: "mapbox.dark",
+    accessToken: API_KEY
+  });
+
+  // Define baseMap object and create layers
+
+  var baseMaps = {
+    "Satelite Map": satmap,
+    "Dark Map": darkmap
+  };
+
+  // Create overlay object to hold our overlay layer
+  var overlayMaps = {
+    Earthquakes: earthquakes
+  };
+
+  //Create map and give it layers
+
+  var myMap = L.map("map", {
+    center: [30,-100],
+    zoom: 3,
+    layers: [satmap, earthquakes]
+  });
+
+  // Layer control and passing in basemaps and overlay
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+
+}
