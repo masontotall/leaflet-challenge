495c9d3d08fc53e95b3430ab8718ede6627fdb5a
masontotall <68935404+masontotall@users.noreply.github.com>
2021-06-13 18:03:32 -0500

    Still cleaning up issues

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -34,7 +34,7 @@ createMap(earthquakes);
 
 function createMap(earthquakes) {
 
-    // Satelite and Darkmap layers
+    // Satelite layer
 
     var satmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
